37679f81c42ee8c087d1a93f24d25a774f3edab7
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-03 19:57:25 +0800

    Apply PHP CS Fixer

--- config/wp2l.php
+++ config/wp2l.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'domain'				  => env('WP2L_DOMAIN'),
+    'domain'                  => env('WP2L_DOMAIN'),
     'default_author_name'     => env('WP2L_DEFAULT_AUTHOR_NAME'),
     'default_author_email'    => env('WP2L_DEFAULT_AUTHOR_EMAIL'),
     'default_author_password' => 'secret',
