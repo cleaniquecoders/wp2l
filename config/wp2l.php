be4c88a4157c06fb5524f1fc3a24296909a852c2
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-03 19:55:52 +0800

    Set ENV for wp2l

--- config/wp2l.php
+++ config/wp2l.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'default_author_name'     => 'Nasrul Hazim',
-    'default_author_email'    => 'nasrulhazim.m@gmail.com',
+	'domain'				  => env('WP2L_DOMAIN'),
+    'default_author_name'     => env('WP2L_DEFAULT_AUTHOR_NAME'),
+    'default_author_email'    => env('WP2L_DEFAULT_AUTHOR_EMAIL'),
     'default_author_password' => 'secret',
     'types'                   => ['posts', 'pages', 'categories', 'comments', 'tags', 'media'],
 ];
